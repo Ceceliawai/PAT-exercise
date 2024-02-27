ac5bc26cbeb96595f5f72820d649f000cfab17e2
hinse <2570877057@qq.com>
2024-02-27 11:08:00 +0800

    1079代码格式化

--- 1079 Total Sales of Supply Chain/TotalSalesofSupplyChain.cpp
+++ 1079 Total Sales of Supply Chain/TotalSalesofSupplyChain.cpp
@@ -1,42 +1,41 @@
+#include <stdio.h>
+
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <stdio.h>
 using namespace std;
-vector<int>n[100010];
+vector<int> n[100010];
 int numofpro[100010];
-int N,K,curid;
-double P,r,total=0;
-void dfs(int root,double curp){
-    double newp = root!= 0 ? curp*(1+r) : curp;
-    if(n[root].size()==0){
-        total+=newp*numofpro[root];
+int N, K, curid;
+double P, r, total = 0;
+void dfs(int root, double curp) {
+    double newp = root != 0 ? curp * (1 + r) : curp;
+    if (n[root].size() == 0) {
+        total += newp * numofpro[root];
         return;
-    }
-    else{
-        for(int i=0;i<n[root].size();i++){
-            dfs(n[root][i],newp);
+    } else {
+        for (int i = 0; i < n[root].size(); i++) {
+            dfs(n[root][i], newp);
         }
     }
     return;
 }
-int main(){
-    cin>>N>>P>>r;
-    r=r/100;//转换成百分数
-    fill(numofpro,numofpro+100010,0);
-    for(int i=0;i<N;i++){
-        cin>>K;
-        if(K!=0){
-            for(int j=0;j<K;j++){
-                cin>>curid;
+int main() {
+    cin >> N >> P >> r;
+    r = r / 100;  // 转换成百分数
+    fill(numofpro, numofpro + 100010, 0);
+    for (int i = 0; i < N; i++) {
+        cin >> K;
+        if (K != 0) {
+            for (int j = 0; j < K; j++) {
+                cin >> curid;
                 n[i].push_back(curid);
             }
-        }
-        else{
-            cin>>curid;
-            numofpro[i]=curid;
+        } else {
+            cin >> curid;
+            numofpro[i] = curid;
         }
     }
-    dfs(0,P);
-    printf("%.1f",total);
+    dfs(0, P);
+    printf("%.1f", total);
 }

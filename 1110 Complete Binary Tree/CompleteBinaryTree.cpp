783fcc4dc5a87978a320b523dfac64a42706527d
hinse <2570877057@qq.com>
2024-03-06 20:05:18 +0800

    1110未AC，但不知道怎么de了

---
+++ 1110 Complete Binary Tree/CompleteBinaryTree.cpp
@@ -0,0 +1,68 @@
+#include <string.h>
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+int v[25][2];
+bool e[25];
+int N;
+int main() {
+    cin >> N;
+    string s;
+    fill(e, e + 25, false);
+    fill(v[0], v[0] + 25 * 2, -1);
+    for (int i = 0; i < N; i++) {
+        cin >> s;
+        if (s != "-") {
+            v[i][0] = stoi(s);
+            e[stoi(s)] = true;
+        }
+        cin >> s;
+        if (s != "-") {
+            v[i][1] = stoi(s);
+            e[stoi(s)] = true;
+        }
+    }
+    int root;
+    for (int i = 0; i < N; i++) {
+        if (e[i] == false) {
+            root = i;
+            break;
+        }
+    }
+    bool flag = true;
+    bool onlyleft = false;
+    queue<int> q;
+    q.push(root);
+    int cur;
+    while (q.empty() == false) {
+        cur = q.front();
+        q.pop();
+        if (onlyleft == true) {
+            if (v[cur][0] != -1 || v[cur][1] != -1) {
+                flag = false;
+                break;
+            }
+        }
+        if (v[cur][0] == -1 && v[cur][1] != -1) {
+            flag = false;
+            break;
+        }
+        if (v[cur][0] != -1 && v[cur][1] == -1) {
+            onlyleft = true;
+            // 从这之后的必须是叶子节点
+        }
+        if (v[cur][0] != -1) q.push(v[cur][0]);
+        if (v[cur][1] != -1) q.push(v[cur][1]);
+    }
+    if (flag == false) {
+        cout << "NO " << root << endl;
+    } else {
+        cout << "YES ";
+        // 层次遍历
+        cout << cur;
+    }
+    return 0;
+}

9b7e13e958078d04b65620896915c6e450224e41
hinse <2570877057@qq.com>
2024-03-08 13:15:45 +0800

    ac

--- 1117 Eddington Number/Enum.cpp
+++ 1117 Eddington Number/Enum.cpp
@@ -3,33 +3,16 @@
 using namespace std;
 int N;
 int days[100010];
+bool mycmp(int a,int b){
+    return a>b;
+}
 int main() {
     cin >> N;
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
         cin >> days[i];
     }
-    sort(days, days + N);
-    int curd = days[N - 1];  // 当前的更大的miles
-    int lastd;
-    int cnt = 0;     // 当前的比curd更大的天数
-    int curcnt = 1;  // 骑行等于curd的天数
-    for (int i = N - 2; i >= 0; i--) {
-        int E = N - i - 1;
-        if (days[i] == curd) {
-            curcnt++;
-        } else {
-            lastd = curd;
-            curd = days[i];
-            cnt += curcnt;
-            curcnt = 1;
-        }
-        // 比curd更大的天数有curd天
-        if (curd == cnt) {
-            cout << cnt;
-            return 0;
-        }
-        if (curd < cnt && lastd > cnt) {
-            cout << cnt;
-        }
-    }
+    sort(days + 1, days + 1 + N,mycmp);
+    int i=1;
+    while(days[i]>i)i++;
+    cout<<i-1;
 }
